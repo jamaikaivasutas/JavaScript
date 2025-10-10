9b0a96ba0fecfd9ee14e56eccf8091b71a1ec5ac
jamaikaivasutas <b1duzale@vasvari.org>
2025-10-10 09:35:43 +0200

    blog

--- 13. osztály/blog/app.js
+++ 13. osztály/blog/app.js
@@ -1,23 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import postsRoutes from './routes/postsRoutes';
-import usersRoutes from './routes/usersRoutes';
-import user from "./data/user.js";
+import express from "express";
+import cors from "cors";
+import * as postsRoutes from "./routes/postsRoutes.js";
+import * as usersRoutes from "./routes/usersRoutes.js";
 
 const PORT = 3000;
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.use("/api", usersRoutes);
+app.use("/api", postsRoutes);
 
-
-app.listen(PORT, () =>  {
-    console.log(`Server runs on port ${PORT}`);
-}) 
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server runs on port ${PORT}`);
+});
--- 13. osztály/blog/routes/postsRoutes.js
+++ 13. osztály/blog/routes/postsRoutes.js
@@ -1,36 +1,58 @@
-import { Router } from 'express';
+import { Router } from "express";
 import * as db from "../data/post.js";
-
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Posts');
+router.get("/", (req, res) => {
+  res.send("Posts");
 });
 
-router.get("/posts", (req ,res) => {
-    const posts = db.getPosts()
-    res.json(posts);
-})
+router.get("/posts", (req, res) => {
+  const posts = db.getPosts();
+  res.json(posts);
+});
 
-router.get("/posts/:id", (req ,res) => {
-    const post = db.getPostById(+req.params.id);
-    if(!post){
-        return res.status(404).json({message: "Post not found"});
-    }
-    res.json(post);
-})
+router.get("/posts/:id", (req, res) => {
+  const post = db.getPostById(+req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  res.json(post);
+});
 
 router.post("/posts", (req, res) => {
-    const {title, content} = req.body;
-    if(!title || !content){
-        return res.status(400).json({message: "Post title or content cannot be empty"});
-    }
-    const saved = db.savePost(title, content)
-    const post = db.getPostById(saved.lastInsertRowid)
-    res.status(201).json(post);
-})
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res
+      .status(400)
+      .json({ message: "Post title or content cannot be empty" });
+  }
+  const saved = db.savePost(title, content);
+  const post = db.getPostById(saved.lastInsertRowid);
+  res.status(201).json(post);
+});
 
-router.
+router.put("/posts/:id", (req, res) => {
+  const post = db.getPostById(+req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  db.updatePost(id, userId, title, content);
+  const updatedPost = db.getPostById(id);
+  res.status(200).json(updatedPost);
+});
+
+router.delete("/posts/:id", (req, res) => {
+  const post = db.getPostById(+req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  const result = db.deletePost(+req.params.id);
+  res.status(200).json(result);
+});
 
 export default router;
